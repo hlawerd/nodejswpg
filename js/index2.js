5131d3558e667d68377b6310f4a800ef18c60bb6
Hugo Leonardo <hlawerd@gmail.com>
2018-04-03 21:06:21 -0400

    finished index2.js

--- js/index2.js
+++ js/index2.js
@@ -3,5 +3,5 @@ var firstname, lastapelido, Age, address, DateofBirth;
 firstname = 'Hugo';
 lastapelido = 'Gomes de Lima';
 age = 39;
-DateofBirth = 15/12/78;
+DateofBirth = '12/15/1978';
 address = '3 Donald Street ';
