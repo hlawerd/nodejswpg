1d4e297bc2dfb1ec891effd4a97f2c8aef4f0173
Hugo Leonardo <hlawerd@gmail.com>
2018-04-23 13:00:48 -0500

    finished the exercise 44 js

--- js/index44.js
+++ js/index44.js
@@ -1,7 +1,11 @@
-let text    =   'game of thrones';
-let firstChar   = text.charAt(0) ;
-let secondChar  = text.charAt(5);
-let thirdChar   = text.charAt(8);
-let name        = firstChar.concat(secondChar,thirdChar);
+let text    =   'HELLO';
+let firstChar   = text.slice(4,5) ;
+let secondChar  = text.slice(3,4);
+let thirdChar   = text.slice(2,3);
+let fourthChar  = text.slice(1,2);
+let fivethChar  = text.slice(0,1);
+let result      = firstChar.concat(secondChar,thirdChar,fourthChar,fivethChar);
 
-console.log(`The final result is: ${name.toUpperCase()}`);
+
+
+console.log(`The final result is: ${result.toLowerCase()}`);
