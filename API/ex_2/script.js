5933ffb911c93fee1a996e4b81b8bebc3b6e643a
Hugo Leonardo <hlawerd@gmail.com>
2018-05-09 15:29:47 -0500

    finished ex 10 to 13

---
+++ API/ex_2/script.js
@@ -0,0 +1,10 @@
+/* Using the console show odd numbers from 1 to 1000 */
+
+let numbers = 1;
+
+for (numbers; (numbers <= 1000); numbers++){
+    if (numbers % 2 === 1) {
+        console.log(numbers);
+    }
+    
+}
---
+++ API/ex_7/teste.js
@@ -0,0 +1,9 @@
+const array = [];
+
+
+for (let i = 0; i <= 4; i++){
+    array[i] =  5;
+     let soma = array[i];
+     let total = soma/ array.length;
+    console.log(`${array[i]}`);
+}
